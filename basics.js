f28dd0752c524fca5e80665c36c21b286565e94b
Muhammad Atif <Atifmastermind12@gmail.com>
2024-01-12 19:19:10 +0500

    added files and modified index file

--- basics.js
+++ basics.js
@@ -1,11 +1,35 @@
 // alert
-alert("Good Job!");
+// alert("Good Job!");
 
 // Calculation
 
-2 + 2;
-10 - 3;
+// 2 + 2;
+// 10 - 3;
 
 // to replace everything on page and write that text (its called mudifiing the web page its one of the most importent feature of javascript )
 
-document.body.innerHTML = "Hellow";
+// document.body.innerHTML = "Hellow";
+
+// numbers and math
+
+//syntax
+/*
+rules that we have to follow when using a prgrammng language
+similar to english (grammar) we can create write as long as user understand its ok
+but in programming we have to follow the rules of syntax exactly
+*/
+
+// tutorial
+// exp1
+// let tshirt = 10;
+// let socks = 8;
+// let dinner = 20;
+// console.log(tshirt + socks + dinner);
+
+// exp2
+// let bankaccount = 100;
+// let lunch = 20;
+// let dinner = 50;
+// let salary = 200;
+
+// console.log(bankaccount - lunch - dinner + salary);
