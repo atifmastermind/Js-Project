cb3509f109b4d53a9dd6582bdb06edc5eb19a6f1
Muhammad Atif <Atifmastermind12@gmail.com>
2024-01-12 14:57:40 +0500

    added files and modified index file

---
+++ basics.js
@@ -0,0 +1,11 @@
+// alert
+alert("Good Job!");
+
+// Calculation
+
+2 + 2;
+10 - 3;
+
+// to replace everything on page and write that text (its called mudifiing the web page its one of the most importent feature of javascript )
+
+document.body.innerHTML = "Hellow";
